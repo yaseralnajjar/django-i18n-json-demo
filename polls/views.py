1c02821e67d73f086268b17431e31bde7ff3a6f1
0xRumple <kasseryasser@windowslive.com>
2018-11-09 11:39:56 +0200

    Fix typo locale

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 import i18n
 i18n.set('locale', 'ar')
-i18n.load_path.append('local')
+i18n.load_path.append('locale')
 
 def index(request):
     return HttpResponse(i18n.t('foo.hi'))
